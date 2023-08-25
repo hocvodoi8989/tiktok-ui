ead61f5117ae00f44ab226b15291bda2574e4092
Do Nhu Hoc <hocvodoi9898@gmail.com>
2023-08-25 15:46:59 +0700

    update1

--- src/components/Layout/component/Header/index.js
+++ src/components/Layout/component/Header/index.js
@@ -1,12 +1,26 @@
 import classNames from 'classnames/bind';
-import styles from './Header.module.scss'
 
-const cx = classNames.bind(styles)
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+
+const cx = classNames.bind(styles);
 
 function Header() {
-    return <header className={cx('wrapper')}>
-        <div className={cx('inner')}></div>
-    </header>
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search" />
+                    <button className={cx('clear')}></button>
+                    {/* loading */}
+                    <button className={cx('search-btn')}>{/* Search */}</button>
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
