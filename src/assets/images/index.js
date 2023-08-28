cf883060cd58cbb97ee750efee7ec4f6e48c0990
Do Nhu Hoc <hocvodoi9898@gmail.com>
2023-08-28 16:04:19 +0700

    abc

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,6 @@
 const images = {
     logo: require('~/assets/images/logo.svg').default,
+    user: require('~/assets/images/user.jpeg'),
 };
 
 export default images;
--- src/components/Layout/component/Header/index.js
+++ src/components/Layout/component/Header/index.js
@@ -1,4 +1,8 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faPlus, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faTelegram } from '@fortawesome/free-brands-svg-icons';
+import { faMessage } from '@fortawesome/free-regular-svg-icons';
 
 import styles from './Header.module.scss';
 import images from '~/assets/images';
@@ -14,9 +18,23 @@ function Header() {
                 </div>
                 <div className={cx('search')}>
                     <input placeholder="Search" />
-                    <button className={cx('clear')}></button>
-                    {/* loading */}
-                    <button className={cx('search-btn')}>{/* Search */}</button>
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('action')}>
+                    <button>
+                        <FontAwesomeIcon className={cx('plus')} icon={faPlus} />
+                        <span className={cx('upload')}>Upload</span>
+                    </button>
+                    <FontAwesomeIcon className={cx('telegram')} icon={faTelegram} />
+                    <FontAwesomeIcon className={cx('inbox')} icon={faMessage} />
+                    <img className={cx('user')} src={images.user} alt="user" />
                 </div>
             </div>
         </header>
